d99a2c027165c8257b6f9dda0f200cefe5fc41ea
Maxim Brishten <b3tmanann@gmail.com>
2023-05-11 10:49:14 +0300

    Chapter 10 done.

---
+++ Chapter 10. List/SwiftUIList/SwiftUIList/SwiftUIListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIListApp.swift
+//  SwiftUIList
+//
+//  Created by Maxim Brishten on 11.05.23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
